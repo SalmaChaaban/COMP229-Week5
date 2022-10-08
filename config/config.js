ae709c7b67608e0c9499ac00b7dd3c150e19b5ce
Salma Chaaban <schaaban@my.centennialcollege.ca>
2022-10-09 02:09:19 +0300

    Mongo Atlas setup completed

--- config/config.js
+++ config/config.js
@@ -1,2 +1,3 @@
 export const Secret = "MySecret";
-export const MongoURI  = "mongodb://127.0.0.1/media"
+// export const MongoURI  = "mongodb://127.0.0.1/media"
+export const MongoURI  = "mongodb+srv://Salma:salma123@comp229.wjzgekh.mongodb.net/?retryWrites=true&w=majority"
