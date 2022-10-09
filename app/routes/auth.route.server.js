a7c16e5b672dfb975211cce9466e3de4ab8050d0
Salma Chaaban <schaaban@my.centennialcollege.ca>
2022-10-09 03:47:30 +0300

    Authentication Completed

--- app/routes/auth.route.server.js
+++ app/routes/auth.route.server.js
@@ -1,12 +1,24 @@
 import { Router } from "express";
-import { DisplayLoginPage, DisplayRegisterPage } from "../controllers/auth.controller.server.js";
+import { DisplayLoginPage, 
+    DisplayRegisterPage, 
+    ProcessRegisterPage, 
+    ProcessloginPage,
+    ProcessLogoutPage} 
+    from "../controllers/auth.controller.server.js";
 
 const router = Router();
 
-// Display login route
+// Display Login Router
 router.get('/login', DisplayLoginPage);
+// Process Login Router
+router.post('/login', ProcessloginPage);
 
-// Display register router
+// Display Register Router
 router.get('/register', DisplayRegisterPage);
+// Process Registration Router
+router.post('/register', ProcessRegisterPage);
+
+// Process Logout
+router.get('/logout', ProcessLogoutPage);
 
 export default router;
