1b3bf0f152e8710126ae2532d7f56963a7610a35
Salma Chaaban <schaaban@my.centennialcollege.ca>
2022-10-09 04:25:12 +0300

    Authorization Completed

--- app/routes/movies.route.server.js
+++ app/routes/movies.route.server.js
@@ -6,14 +6,15 @@ import {  DisplayMoviesList,
     ProcessMoviesEditPage, 
     DisplayMoviesEditPage, 
     ProcessMoviesDelete } from "../controllers/movies.controller.server.js";
+import { AuthGuard } from "../utils/index.js";
 
 const router = Router();
 
 router.get('/movie-list', DisplayMoviesList);
-router.get('/movie-add', DisplayMoviesAddPage);
-router.post('/movie-add', ProcessMoviesAddPage);
-router.post('/movie-edit/:id', ProcessMoviesEditPage);
-router.get('/movie-edit/:id', DisplayMoviesEditPage);
-router.get('/movie-delete/:id', ProcessMoviesDelete);
+router.get('/movie-add', AuthGuard, DisplayMoviesAddPage);
+router.post('/movie-add', AuthGuard, ProcessMoviesAddPage);
+router.post('/movie-edit/:id', AuthGuard, ProcessMoviesEditPage);
+router.get('/movie-edit/:id', AuthGuard, DisplayMoviesEditPage);
+router.get('/movie-delete/:id', AuthGuard, ProcessMoviesDelete);
 
 export default router;
--- app/utils/index.js
+++ app/utils/index.js
@@ -1,12 +1,13 @@
-export function AuthGuard(req, res, next){
-    if(!req.isAuthenticated()){
-        return res.redirect('/login');
-    }
-}
-
 export function UserDisplayName(req){
     if(req.user){
         return req.user.displayName;
     }
     return '';
+}
+
+export function AuthGuard(req, res, next){
+    if(!req.isAuthenticated()){
+        return res.redirect('/login');
+    }
+    next();
 }
